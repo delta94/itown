d479911529febfe0d7b11a48c3d44273fbf7c3b2
talha105 <talhakhan105@yahoo.com>
2021-01-13 18:42:00 +0500

    app

---
+++ src/components/ui/TextHeavy.js
@@ -0,0 +1,15 @@
+// https://stackoverflow.com/questions/35255645/how-to-set-default-font-family-in-react-native
+import React from 'react';
+import {
+	Text,
+	useColorScheme,
+} from 'react-native';
+import {AllHtmlEntities} from 'html-entities';
+import getThemedColors from '../../helpers/Theme';
+import {boldFontFamily} from '../../constants/Colors';
+export default props => {
+	const tval = (typeof props.children === 'string' || props.children instanceof String) ? props.children : ''+props.children;
+	const colors = getThemedColors(useColorScheme())
+	const entities = new AllHtmlEntities();
+	return <Text {...props} style={[{fontFamily: boldFontFamily,color: colors.heavyText}, props.style]}>{entities.decode(tval)}</Text>
+} 
---
+++ src/constants/SiteDetails.js
@@ -0,0 +1,13 @@
+export default {
+	app_key: '80bdfcdb-59da-40ba-bb90-52a1d590ecfa',
+	// TdBezB1nU2SbcF2NwsIXHBotsf2OiY
+
+	url: 'https://itownmadrid.es',
+	terms_page: 'https://townhub.cththemes.com/help-center/',
+}
+
+export const defaultLanguage = {
+	code: 'es',
+	rtl: false
+}
+export const defaultCurrency = '€';
